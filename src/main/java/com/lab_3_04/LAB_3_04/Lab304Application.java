910d4ebd608966c3a1bd760de10d9268e63b1562
mariiahuzo <d.mariya.d@gmail.com>
2022-08-15 22:51:50 +0200

    First commit

---
+++ src/main/java/com/lab_3_04/LAB_3_04/Lab304Application.java
@@ -0,0 +1,13 @@
+package com.lab_3_04.LAB_3_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab304Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab_3_04/LAB_3_04/Lab304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab_3_04.LAB_3_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab304ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
